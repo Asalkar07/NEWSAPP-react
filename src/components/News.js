b91a0c5fff578026931ef5072377e662e96c6093
Asalkar07 <ankit123asalkar@gmail.com>
2024-05-12 13:36:19 +0530

    added function

--- src/components/News.js
+++ src/components/News.js
@@ -88,7 +88,7 @@ const News = (props)=> {
     setPage(page+1)
     let data = await fetch(url);
     let parsedData = await data.json();
-    // setArticles(articles.concat(parsedData.articles))
+    setArticles(articles.concat(parsedData.articles))
     setArticles((prevArticles) => {
       if (prevArticles) {
         return prevArticles.concat(parsedData.articles);
